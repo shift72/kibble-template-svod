87612b2f24c4cf1c8289105072479e0920c4340a
Graham Scragg <gee@null.co.nz>
2020-01-21 11:26:40 +1300

    tidy up styles and classification usage

--- site/static/scripts/main.js
+++ site/static/scripts/main.js
@@ -1,6 +1,3 @@
 document.addEventListener('s72loaded', function(){
-  let app = event.detail.app;
-
-  // Load the classifications
-  app.classificationsService.load('/classifications.all.json');
+  event.detail.app.classificationsService.load('/classifications.all.json');
 });
